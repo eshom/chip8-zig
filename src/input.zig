ee242450c048e9d79239b38886bf37c2eb52fa91
eshom <48328872+eshom@users.noreply.github.com>
2024-10-11 15:42:18 +0300

    feat: keypad

--- src/input.zig
+++ src/input.zig
@@ -1,3 +1,27 @@
+const std = @import("std");
 const rl = @import("raylib.zig").raylib;
 
 pub const pollInputEvents = rl.PollInputEvents;
+pub const getKeyPressed = rl.GetKeyPressed;
+
+pub const Keypad = enum(c_int) {
+    one = rl.KEY_ONE,
+    two = rl.KEY_TWO,
+    three = rl.KEY_THREE,
+    c = rl.KEY_FOUR,
+
+    four = rl.KEY_Q,
+    five = rl.KEY_W,
+    six = rl.KEY_E,
+    d = rl.KEY_R,
+
+    seven = rl.KEY_A,
+    eight = rl.KEY_S,
+    nine = rl.KEY_D,
+    e = rl.KEY_F,
+
+    a = rl.KEY_Z,
+    zero = rl.KEY_X,
+    b = rl.KEY_C,
+    f = rl.KEY_V,
+};
